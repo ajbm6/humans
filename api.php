8c651f2866c80fd6f7afc4c97345fcc1fc558108
geoffreyhale <geoffreyhale@gmail.com>
2016-03-17 18:30:14 -0700

    fix api for POST ticks

--- api.php
+++ api.php
@@ -6,10 +6,11 @@ require_once 'api.class.php';
 $db = new Database();
 $api = new Api($db);
 
-if (isset($_GET['ticks'])) {
-    $username = isset($_GET['username']) ? $_GET['username'] : '';
-    $ticks = $_GET['ticks'];
-    $api->saveScore($username, $ticks);
+if (isset($_POST['ticks'])) {
+    $username = isset($_POST['username']) ? $_POST['username'] : '';
+    $ticks = $_POST['ticks'];
+    $result = $api->saveScore($username, $ticks);
+    echo $result;
 }
 
 if (isset($_GET['action']) && $_GET['action'] == 'get_scores') {
