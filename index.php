67bf2da3bc7d072dc6fada5feeca370b8c343200
geoffreyhale <geoffreyhale@gmail.com>
2016-03-19 00:15:18 -0700

    improve help section

--- index.php
+++ index.php
@@ -119,7 +119,7 @@
                 <div class="btn-group btn-group-xs">
                     &nbsp;
                 </div>
-                <div>Ticks of time</div>
+                <div>Ticks</div>
             </div>
         </div>
 
@@ -146,45 +146,44 @@
                         <h3 class="panel-title">Help</h3>
                     </div>
                     <div class="panel-body">
-                        <p>Humans reproduce and consume food.  Humans need food or they die.</p>
+                        <ul style="list-style-type: none; padding-left: 0;">
+                            <li><i class="fa fa-users fa-fw" ></i> Humans need <i class="fa fa-apple fa-fw" ></i> food or they <span style="color:red;">-1<i class="fa fa-user fa-fw" ></i></span> die.</li>
+                            <li><i class="fa fa-users fa-fw" ></i> Humans each consume <span style="color:red;">-1<i class="fa fa-apple fa-fw" ></i></span> food per <i class="fa fa-clock-o fa-fw" ></i> tick.</li>
+                            <li><i class="fa fa-users fa-fw" ></i> Humans farm <span style="color:green;">+<i class="fa fa-apple fa-fw" ></i></span> food.</li>
+                            <li><i class="fa fa-users fa-fw" ></i> Humans reproduce <span style="color:green;">+<i class="fa fa-user fa-fw" ></i></span> humans.</li>
+                            <li><i class="fa fa-users fa-fw" ></i> Humans collect <span style="color:green;">+<i class="fa fa-tree fa-fw" ></i></span> wood.</li>
+                            <li><i class="fa fa-home fa-fw" ></i> Homes cost <span style="color:red;">40<i class="fa fa-tree fa-fw" ></i></span> wood and allow <span style="color:green;">+4<i class="fa fa-users fa-fw" ></i></span> human capacity.</li>
+                            <li><i class="fa fa-users fa-fw" ></i> Humans collect <span style="color:green;">+<i class="fa fa-bitcoin fa-fw" ></i></span> bitcoin.</li>
+                            <li><em>Fastest (fewest <i class="fa fa-clock-o fa-fw" ></i> ticks) to 1,000 <i class="fa fa-bitcoin fa-fw" ></i></span> bitcoin wins!</em></li>
+                        </ul>
+                        <hr>
                         <h4>Getting Started</h4>
                         <ol>
                             <li>
-                                You start with 3 Humans. 2 are Farming. 1 is Idle.<br>
-                                <strong>Action:</strong> Put the Idle Human to work Farming by clicking on the green plus (+) next to Farming.<br>
-                                Notice your Food increasing more quickly.  With your 3 Farmers, you produce +6 food/tick, but humans eat 1 food/tick so the net change is +3 food/tick (indicated in green above).
+                                You start with 3 <i class="fa fa-users fa-fw" ></i> humans. 2 are farming. 1 is unemployed. <strong>Click on the <button type="button" class="btn btn-xs btn-success"><i class="fa fa-plus"></i></button> green plus button next to farming</strong> to give the unemployed human a job.  Notice your <span style="color:green;">+<i class="fa fa-apple fa-fw" ></i></span> food increasing more quickly.  3 Farmers produce +6 food/tick, but humans eat 1 food/tick.  Your net food change is +3 food/tick.
                             </li>
                             <li>
-                                When you build up enough food, you can send a couple farmers to Sexing for a few seconds.<br>
-                                <strong>Action:</strong> Click the red (-) next to Farming twice.  Click the green (+) next to Sexing twice.<br>
-                                Notice a new Human is produced!<br>
-                                <strong>Action:</strong> Quickly put everyone back on farming before you starve and die!
+                                When you have enough <i class="fa fa-apple fa-fw" ></i> food, you can send a couple farmers to reproduce for a few <i class="fa fa-clock-o fa-fw" ></i> ticks.  <strong>Click the <button type="button" class="btn btn-xs btn-danger"><i class="fa fa-minus"></i></button> red minus button next to Farming twice.  Click the <button type="button" class="btn btn-xs btn-success"><i class="fa fa-plus"></i></button> green plus button next to Reproducing twice.</strong> Notice a new <span style="color:green;">+1<i class="fa fa-user fa-fw" ></i></span> human is produced.  <strong>Quickly put everyone back on farming</strong> before you starve and <span style="color:red;">-1<i class="fa fa-user fa-fw" ></i></span> die!
                             </li>
                             <li>
-                                You are maxed at 4 Humans because you only have 1 Home.  Notice Homes have a capacity of 4 humans and cost 40 wood to build.  For more Humans we need to build another Home.  Let's get some Wood.<br>
-                                <strong>Action:</strong> Put a couple workers on Lumbering.<br>
+                                You are maxed at 4 <i class="fa fa-users fa-fw" ></i> humans because you only have 1 <i class="fa fa-home fa-fw" ></i> home.  For more humans we need to build another Home.  We need wood to build a home.  <strong>Put a couple workers on lumbering</strong>.  When we get 40 <i class="fa fa-tree fa-fw" ></i> wood we can <strong>build another <span style="color:green;">+1<i class="fa fa-home fa-fw" ></i></span> home</strong>.
                             </li>
                             <li>
-                                When we get 40 Wood we can build another Home.  Then we can have more Humans.<br>
-                                <strong>Action:</strong> Build a Home.
+                                Build up your population. <strong> Put a couple humans on reproduction</strong>.  Keep building <i class="fa fa-home fa-fw" ></i> homes and build up to a steady flow of new +<i class="fa fa-user fa-fw" ></i> humans.  Satisfy the increasing <i class="fa fa-apple fa-fw" ></i> food demand.
                             </li>
                             <li>
-                                Now that you're getting the hang of it, let's build up a steady population growth.<br>
-                                <strong>Action:</strong> Put a couple humans on Sexing.<br>
-                            </li>
-                            <li>
-                                Now you've got a steady flow of humans being produced, an increasing food demand, and a goal of building homes for an ever increasing population cap.
-                                <strong>Action:</strong> Try to achieve a population of 1,000 Humans!<br>
-                                Note: Increase the multiplier when you want to move more Humans per click!
+                                Achieve 1,000 <i class="fa fa-bitcoin fa-fw" ></i> bitcoin as <i class="fa fa-clock-o fa-fw" ></i> quickly as possible!
                             </li>
                         </ol>
+                        You can increase the multiplier when you want to move more <i class="fa fa-users fa-fw" ></i> humans per click
                     </div>
                 </div>
             </div>
             <div class="col-xs-6">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        High Scores
+                        <h3 class="panel-title">High Scores</h3>
+                        <em>fastest (least ticks) to 1,000 <i class="fa fa-bitcoin fa-fw" ></i> Bitcoin</em>
                     </div>
                     <div class="panel-body">
                         <div id="highscores">
